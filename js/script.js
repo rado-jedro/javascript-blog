e3c2f8ce4defa18c77b5ac35125a9c3c8382fa16
Rado Jedro <radoslaw.jedro@gmail.com>
2019-08-15 21:26:46 +0100

    Update script.js with new comments [DONE]

--- js/script.js
+++ js/script.js
@@ -2,15 +2,16 @@
     console.log('Link was clicked!');
     console.log(event);
 
-    /* remove class 'active' from all article links  */
+    /* [DONE] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
 
     for(let activeLink of activeLinks){
       activeLink.classList.remove('active');
     }
     /* add class 'active' to the clicked link */
+    console.log('clickedElement:', clickedElement);
   
-    /* remove class 'active' from all articles */
+    /* [DONE] remove class 'active' from all articles */
     const activeArticles  = document.querySelectorAll('.post');
 
     for(let activeArticle of activeArticles){
@@ -30,6 +31,8 @@
     link.addEventListener('click', titleClickHandler);
   }
 
+
+  //test
   document.getElementById('test-button').addEventListener('click', function(){
     const post = document.querySelectorAll('.post');
     console.log('posts:', post);
