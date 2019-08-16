b25b571dc2eecc3ec0c21003fb2ccb3f8fdf03a2
Rado Jedro <radoslaw.jedro@gmail.com>
2019-08-16 21:51:36 +0100

    Update script.js with structure of generateTitleLinks function

--- js/script.js
+++ js/script.js
@@ -46,9 +46,39 @@
     link.addEventListener('click', titleClickHandler);
   }
 
+  const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+  function generateTitleLinks(){
+
+  /* remove contents of titleList */
+
+  /* for each article */
+
+    /* get the article id */
+
+    /* find the title element */
+
+    /* get the title from the title element */
+
+    /* create HTML of the link */
+
+    /* insert link into titleList */
+
+}
+
+generateTitleLinks();
+
 
   //test
   document.getElementById('test-button').addEventListener('click', function(){
     const post = document.querySelectorAll('.post');
     console.log('posts:', post);
+  
+  
+  
+  
+  
+  
   });
