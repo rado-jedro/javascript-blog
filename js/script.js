4de22a5aa21fe3a71eb4f6a7fc007bebdc5e9f03
Rado Jedro <radoslaw.jedro@gmail.com>
2019-08-16 16:43:21 +0100

    Add to script.js file class 'active' to the clicked link

--- js/script.js
+++ js/script.js
@@ -9,8 +9,14 @@
       activeLink.classList.remove('active');
     }
     /* add class 'active' to the clicked link */
+    const clickedElement = this;
     console.log('clickedElement:', clickedElement);
-  
+
+    console.log('clickedElement (with plus): ' + clickedElement);
+    clickedElement.classList.add('active');
+
+    console.log('clickedElement:', clickedElement);
+
     /* [DONE] remove class 'active' from all articles */
     const activeArticles  = document.querySelectorAll('.post');
 
@@ -23,6 +29,7 @@
     /* find the correct article using the selector (value of 'href' attribute) */
   
     /* add class 'active' to the correct article */
+  
   }
   
   const links = document.querySelectorAll('.titles a');
